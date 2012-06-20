39b4043964a8c4bd7710f30a87be3edf0d3c803f
Thorsten Fischer <thorsten@froschi.org>
2012-06-20 15:26:27 +0200

    Added dependencies for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,12 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libcomerr", ">= 0.0.1"
+depends          "libgcrypt", ">= 0.0.1"
+depends          "libgnutls", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+depends          "libk5crypto", ">= 0.0.1"
+depends          "libkrb5", ">= 0.0.1"
+depends          "libtasn", ">= 0.0.1"
+depends          "libxml2", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,17 @@
+include_recipe "libgnutls"
+include_recipe "libgssapi-krb5"
+include_recipe "libxml2"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libcomerr"
+  include_recipe "libgcrypt"
+  include_recipe "libk5crypto"
+  include_recipe "libkrb5"
+  include_recipe "libtasn"
+
   packages |= %w/
     libneon27-gnutls
   /
